bec9d49af6c58aa970ffad3079f8702167ab4055
Rodriguez Victoria <vrodriguezmora@buenosaires.gob.ar>
2023-12-12 11:32:13 -0300

    Componentes Acomodados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContentComponent } from './components/content/content.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { ContentComponent } from '../content/content.component';
+SidebarComponent
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.scss'
+})
+export class ContainerComponent {
+
+}
