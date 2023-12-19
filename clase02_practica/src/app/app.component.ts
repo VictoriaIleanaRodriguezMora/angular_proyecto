c63a1d64369fe99b5cda18f0cda343a23e383873
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-12-18 22:44:34 -0300

    Commit

---
+++ clase02_practica/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarModule } from './sidebar/sidebar.module';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  
+})
+export class AppComponent {
+  title = 'clase02_practica';
+}
