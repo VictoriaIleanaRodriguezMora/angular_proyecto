dabeb8899a849341a2c13be2f80917523fc4965b
Rodriguez Victoria <vrodriguezmora@buenosaires.gob.ar>
2023-12-20 14:11:52 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarModule } from './sidebar/sidebar.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  
 })
 export class AppComponent {
-  title = 'angular_proyecto';
+  title = 'clase02_practica';
 }
