7960efc1387cedb5d6a2dec2d5ee1a4a0008af32
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-01-09 11:07:23 -0300

    renderer

--- src/app/directives/students.directive.ts
+++ src/app/directives/students.directive.ts
@@ -1,16 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStudents]'
 })
 export class StudentsDirective {
 
-  constructor(elementRef: ElementRef) {
+  constructor(elementRef: ElementRef, renderer: Renderer2) {
 
     console.log("directiva inicializada");
     console.log("elementRef ---> ", elementRef);
-
-
+    renderer.setStyle(elementRef.nativeElement, "background-color", "yellow");
+    renderer.setStyle(elementRef.nativeElement, "border-radius", "25px")
   }
 
 }
