f2c512609b6d3bc5f8b5e1940359fc7612904805
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-01-10 14:18:24 -0300

    Ahora no muestra nada, por que el array employees, está vacio

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
+  @Input() employees: string[] = [];  // valor por defecto de array vacio
 
 }
