91b56d85f11b6192f83e28e02e4680d8862a691c
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-01-11 09:33:38 -0300

    a

--- src/app/modules/students/students.module.ts
+++ src/app/modules/students/students.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentsComponent } from '../../components/students/students.component'
 import { StudentsDirective } from '../../directives/students.directive';
+<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
+=======
+
+
+>>>>>>> parent of 8bf4c8c (data binding)
 
 @NgModule({
   declarations: [StudentsComponent, StudentsDirective],
   imports: [
-    CommonModule, FormsModule
+    CommonModule
   ],
   exports: [StudentsComponent]
 })
